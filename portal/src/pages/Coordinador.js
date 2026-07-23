1c7a30a180734a4bfc208861dd23451c55cd3a27
Marcos Chiappa <marcos.chiappa@MacBook-Pro-de-Marcos.local>
2026-07-22 23:05:22 -0300

    fix: remove unused useRef import

--- portal/src/pages/Coordinador.js
+++ portal/src/pages/Coordinador.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { db } from '../firebase';
 import { collection, onSnapshot, doc, updateDoc } from 'firebase/firestore';
 
